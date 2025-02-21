c5013606155f8df598a2ed3817d0a4f9aaaedda5
mikeyboy52 <mikevanlommel2007@gmail.com>
2025-02-21 11:59:03 +0100

    Added Authentication to Program.cs

--- IndProject.WebApi/Program.cs
+++ IndProject.WebApi/Program.cs
@@ -1,16 +1,29 @@
 using IndProject.WebApi.Controllers;
 using IndProject.WebApi.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 var sqlConnectionString = builder.Configuration.GetValue<string>("SqlConnectionString");
 var sqlConnectionStringFound = !string.IsNullOrWhiteSpace(sqlConnectionString);
 builder.Services.AddTransient<WeatherForecastRepository, WeatherForecastRepository>(o => new WeatherForecastRepository(sqlConnectionString));
+builder.Services.AddAuthorization();
+builder.Services
+    .AddIdentityApiEndpoints<IdentityUser>(options =>
+    {
+        options.User.RequireUniqueEmail = true;
+
+    })
+    .AddDapperStores(options =>
+    {
+        options.ConnectionString = sqlConnectionString;
+    });
 var app = builder.Build();
 
 app.MapGet("/", () => $"The API is up . Connection string found: {(sqlConnectionStringFound ? "Found" : "Not Found")}");
@@ -24,7 +37,25 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.MapGroup("/account")
+    .MapIdentityApi<IdentityUser>();
 
-app.MapControllers();
+app.MapPost("/account/logout",
+    async (SignInManager<IdentityUser> signInManager,
+    [FromBody] object empty) =>
+    {
+        if (empty != null)
+        {
+            await signInManager.SignOutAsync();
+            return Results.Ok();
+        }
+        return Results.Unauthorized();
+    }
+    )
+    .RequireAuthorization();
+    
+
+app.MapControllers()
+    .RequireAuthorization();
 
 app.Run();
