fe8b6af801598d272d3719a755ce57de9b5f4159
mikeyboy52 <mikevanlommel2007@gmail.com>
2025-02-26 12:32:01 +0100

    Removed User stuff

--- IndProject.WebApi/Program.cs
+++ IndProject.WebApi/Program.cs
@@ -18,7 +18,6 @@ builder.Services.AddSwaggerGen();
 var sqlConnectionString = builder.Configuration.GetValue<string>("SqlConnectionString");
 var sqlConnectionStringFound = !string.IsNullOrWhiteSpace(sqlConnectionString);
 builder.Services.AddTransient<EnviromentRepository, EnviromentRepository>(o => new EnviromentRepository(sqlConnectionString));
-builder.Services.AddTransient<UserRepository, UserRepository>(o => new UserRepository(sqlConnectionString));
 builder.Services.AddTransient<ObjectRepository, ObjectRepository>(o => new ObjectRepository(sqlConnectionString));
 builder.Services.AddAuthorization();
 builder.Services
